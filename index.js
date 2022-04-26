45bc0d5889c0203d11d390273e703ac8b265113c
jehsjardim <jehsjardim@gmail.com>
2022-04-26 20:19:29 -0300

    atualizai

---
+++ index.js
@@ -0,0 +1,6 @@
+
+let util = require("./uteis");
+
+console.log("Oi gente");
+console.log(util);
+
---
+++ uteis.js
@@ -0,0 +1,12 @@
+module.exports = [
+
+    ehPrimo: function(valor) {
+       for (let i=2; i<valor/2; i++) {
+            if (valor%i == 0) {
+                return false;
+            }
+         }
+          return true;
+    }
+
+]
