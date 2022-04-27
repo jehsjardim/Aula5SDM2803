8872dcdf14ed8b51af957cda1d26d4f1b9de1dff
jehsjardim <jehsjardim@gmail.com>
2022-04-26 21:46:11 -0300

    express iniciado2

--- uteis.js
+++ uteis.js
@@ -2,11 +2,10 @@ module.exports = {
 
     ehPrimo: function (valor) {
         for (let i = 2; i < valor / 2; i++) {
-            if (valor%i == 0) {
+            if (valor % i == 0) {
                 return false;
             }
         }
-        return true;
     }
 
 }
