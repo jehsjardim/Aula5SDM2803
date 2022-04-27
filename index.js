49e902f57e83ab7f7095d0d1238aadd4d296320d
jehsjardim <jehsjardim@gmail.com>
2022-04-26 21:44:00 -0300

    express iniciado

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 
-console.log("Oi gente");
+console.log("🚀🚀🚀 Iniciando meu servidor 🚀🚀🚀");
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/teste', (req, res) => {
+    res.send('Você acessou a rota /teste')
+
+})
+
+app.listen(port,() => {
+    console.log('Servidor iniciado....escutando na porta ${port}')
+})
